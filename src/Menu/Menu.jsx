8c07f1fd443b27b1f353d07a32af22c871242957
Linggos06 <julia.snk@hotmail.com>
2024-03-25 18:42:37 +0300

    Create Menu component

---
+++ src/Menu/Menu.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchNews, selectNews, setQuery } from "../config/store/newsSlice";
+import { generateUrl } from "../config/generateUrl";
+import categories from "../config/categories";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+
+import "./Menu.scss";
+
+const Menu = () => {
+  const dispatch = useDispatch();
+  const { currentSource, query } = useSelector(selectNews);
+  const [activeTab, setActiveTab] = useState("home");
+
+  const handleChange = (event, value) => {
+    if (query) {
+      dispatch(setQuery(""));
+    }
+    setActiveTab(value);
+    const url = generateUrl(value, currentSource);
+    dispatch(fetchNews(url));
+  };
+
+  return (
+    <Box className="menu_container">
+      <Tabs
+        value={query ? false : activeTab}
+        variant="scrollable"
+        scrollButtons
+        allowScrollButtonsMobile
+        onChange={handleChange}
+      >
+        {categories.map(({ label, value }, index) => (
+          <Tab key={index} className="menu_tab" label={label} value={value} />
+        ))}
+      </Tabs>
+    </Box>
+  );
+};
+
+export default Menu;
