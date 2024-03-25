73e5c2ec2dd1f8342c7e3956f7c8121568270780
Linggos06 <julia.snk@hotmail.com>
2024-03-25 18:46:37 +0300

    Create constants

---
+++ src/config/categories.js
@@ -0,0 +1,12 @@
+const categories = [
+  { label: "Home", value: "home" },
+  { label: "World", value: "world" },
+  { label: "Business", value: "business" },
+  { label: "Technology", value: "technology" },
+  { label: "Entertainment", value: "entertainment" },
+  { label: "Sports", value: "sports" },
+  { label: "Science", value: "science" },
+  { label: "Health", value: "health" },
+];
+
+export default categories;
---
+++ src/config/constants.js
@@ -0,0 +1,12 @@
+// News sources
+export const BBC_NEWS = "bbc";
+export const GUARDIAN_NEWS = "guardian";
+export const NYTIMES_NEWS = "nytimes";
+
+// News sources urls
+export const BBC_NEWS_URL = "https://newsapi.org/v2/top-headlines";
+export const GUARDIAN_NEWS_URL = "https://content.guardianapis.com";
+export const NYTIMES_NEWS_URL = "https://api.nytimes.com/svc/topstories/v2";
+export const BBC_NEWS_SEARCH_URL = "https://newsapi.org/v2/everything";
+export const NYTIMES_NEWS_SEARCH_URL =
+  "https://api.nytimes.com/svc/search/v2/articlesearch.json";
